f92d730c062774d5a4fa79701fec99823656651d
Yuliana Smirnova <0387097@gmail.com>
2021-06-11 16:32:55 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostInfo;
+
+public class PostManager {
+    private PostInfo [] postInfos;
+
+    public PostInfo [] search (int ownerId, String domain, String query, boolean ownersOnly, int count, int offers) {
+        return null;
+    }
+
+    public void delete (int ownerId, int postId) {
+
+    }
+}
